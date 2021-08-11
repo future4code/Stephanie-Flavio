8482bb33b499710e37e8563738ba37ad7679cd79
Stephanie Flavio <stephdpmello@gmail.com>
2021-08-11 16:49:01 -0300

    adicionei arquivos na pasta strings-arrays

---
+++ semana1/strings-arrays/strings-arrays.js
@@ -0,0 +1,56 @@
+ 1-
+
+ a. // undefined 
+
+ b. // null
+
+ c. // 11
+
+ d. // 3 
+
+ e. // 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13
+
+ f. // 9
+_________________________________________________________________________________________________________________________________
+ 2-
+
+  //SUBI NUM ÔNIBUS EM MIRROCOS - 27 
+_________________________________________________________________________________________________________________________________
+ 3-
+// const nomeDoUsuario = prompt("Qual seu nome?")
+// const emailDoUsuario = prompt("Digite seu e-mail:")
+
+// console.log
+// ("O e-mail " + emailDoUsuario + " foi cadastrado com sucesso. Seja bem-vindo(a), " + nomeDoUsuario)
+_________________________________________________________________________________________________________________________________
+ 4-
+a)
+//  const comidasFavoritas = ["sorvete", "frango", "purê de batata", "uva", "churrasco"]
+// // console.log(comidasFavoritas)
+b)
+//    const tamanho = comidasFavoritas.lenght
+//   console.log("Estas são as minhas comidas preferidas: ", comidasFavoritas[0])
+//  console.log("Estas são as minhas comidas preferidas: ", comidasFavoritas[1])
+//  console.log("Estas são as minhas comidas preferidas: ", comidasFavoritas[2])
+//  console.log("Estas são as minhas comidas preferidas: ", comidasFavoritas[3])
+//  console.log("Estas são as minhas comidas preferidas: ", comidasFavoritas[4])
+c)
+//  const comidaFavoritaUsuario = prompt("Qual sua comida favorita?")
+ 
+//   minhasComidasFavoritas[1] = comidaFavoritaUsuario
+//   console.log(minhasComidasFavoritas)
+_________________________________________________________________________________________________________________________________
+5-
+a)
+//let listaDeTarefas = []
+b)
+// let tarefasDiarias1 = prompt("Liste 3 tarefas diárias:")
+// let tarefasDiarias2 = prompt("Liste 3 tarefas diárias:")
+// let tarefasDiarias3 = prompt("Liste 3 tarefas diárias:")
+// listaDeTarefas.push(tarefasDiarias1, tarefasDiarias2, tarefasDiarias3)
+c)
+//console.log(listaDeTarefas)
+d)
+// let tarefasRealizadas = Number(prompt("Digite o índice de tarefa realizada: 0 - 1 - 2"))
+e)
+// listaDeTarefas.splice(0, tarefasRealizadas)
