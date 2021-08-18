a74f090c9cc455eee2681e4dc027c3b2a691172d
Stephanie Flavio <stephdpmello@gmail.com>
2021-08-18 16:33:07 -0300

    Adicionei um arquivo na pasta loops

---
+++ semana2/loops/index.js
@@ -0,0 +1,74 @@
+//Exercício 1
+
+//a)
+
+//b) 
+
+// 10
+
+//Exercício 2 
+
+//a)
+
+// 19
+// 21
+// 23
+// 25
+// 27
+// 30
+
+//b)
+
+//Exercício 3
+
+// *
+// **
+// ***
+// ****
+
+_________________________________________________________________________________
+
+//Exercício 1
+
+// const bichinhoEstimacaoUsuario = Number(prompt("Quantos bichinhos de estimação você tem?"))
+
+//     if (bichinhoEstimacaoUsuario === 0){
+//         console.log("Que pena! Você pode adotar um pet!")
+//     } else if (bichinhoEstimacaoUsuario > 0){
+//         let arrayBichinho = []
+//         while (arrayBichinho.length < bichinhoEstimacaoUsuario){
+//             arrayBichinho.push(prompt("Quais os nomes dos seus pets?"))
+//         }
+//         console.log([arrayBichinho])
+//     }
+
+//Exercício 2
+
+// Considere que você tenha acesso a um array  (chamado de 'array original') que é composto somente de números. Baseando-se nisso, crie uma função para cada um dos itens abaixo, realizando as operações pedidas:
+// a) Escreva um programa que imprime cada um dos valores do array original.
+
+
+// const arrayOriginal = [10, 2, 3, 4, 5]
+
+//a)
+
+// for (let i = 0; arrayOriginal.length > i; i++) {
+// console.log(arrayOriginal[i])
+// }
+
+//b)
+
+// for (let i = 0; arrayOriginal.length > i; i++) {
+// console.log(arrayOriginal[i] / 10)
+// }
+
+//c)
+
+// const novoArray = []
+
+// for (let i = 0; arrayOriginal.length > i; i++) {
+// console.log(arrayOriginal[i] % 2 === 0)
+// }
+// 
+
+//INCOMPLETO
