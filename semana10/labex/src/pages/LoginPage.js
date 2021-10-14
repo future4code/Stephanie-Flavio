1b9dc131a41e4b23fb3886c1ada44cdc849f5e23
Stephanie Flavio <stephdpmello@gmail.com>
2021-10-14 20:45:52 -0300

    adicionei metodos axios e autorização no arquivo react

--- semana10/labex/src/pages/LoginPage.js
+++ semana10/labex/src/pages/LoginPage.js
@@ -1,25 +1,64 @@
+import axios from 'axios';
 import React from 'react'
+import { useState } from 'react';
 import { useHistory } from 'react-router';
 
 export function LoginPage() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
   const history = useHistory()
 
-  const goBack = () => {
-    history.goBack()
+  const onChangeEmail= (e) => {
+    setEmail(e.target.value)
   }
 
-  const goToAdminHomePage = () => {
-    history.push("/admin")
+  const onChangePassword= (e) => {
+    setPassword(e.target.value)
+  }
+
+  const onSubmitLogin = () => {
+    console.log(email, password)
+
+    const url = "https://us-central1-labenu-apis.cloudfunctions.net/labeX/stephanie-flavio-maryam/login"
+    const body = {
+      email: email,
+      password: password
+    }
+
+    axios.post(url, body)
+    .then((res) => {
+      console.log(res.data)
+      localStorage.setItem('token', res.data.token)
+      history.push("/")
+    })
+    .catch((err) => {
+      console.log(err.data)
+    })
+  }
+
+
+  const goBack = () => {
+    history.goBack()
   }
 
   return (
     <div>
         <h1>Login</h1>
         <button onClick={goBack}>Voltar</button>
-        <input placeholder="E-mail"/>
-        <input placeholder="Nome"/>
-        <button onClick={goToAdminHomePage}>Enviar</button>
+        <input 
+          placeholder="E-mail"
+          type="email"
+          value={email}
+          onChange={onChangeEmail}
+        />
+        <input
+          placeholder="password"
+          type="password"
+          value={password}
+          onChange={onChangePassword}
+        />
+        <button onClick={onSubmitLogin}>Enviar</button>
     </div>
   );
 }
--- semana10/labex/src/pages/TripsDetailsPage.js
+++ semana10/labex/src/pages/TripsDetailsPage.js
@@ -1,7 +1,38 @@
+import axios from 'axios';
 import React from 'react'
+import { useEffect } from 'react';
 import { useHistory } from 'react-router';
 
+const useProtectedPage = () => {
+  const history = useHistory()
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+
+    if(token === null){
+      console.log('Não está logado!')
+      history.push('/login')
+    }
+  }, )
+}
+
 export function TripsDetailsPage() {
+  useProtectedPage()
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    const url = "https://us-central1-labenu-apis.cloudfunctions.net/labeX/stephanie-flavio-maryam/trip/:id"
+    const headers = {
+      auth: token
+    }
+    axios.get(url, headers)
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch((err) => {
+      console.log(err.response)
+    })
+  }, [])
 
   const history = useHistory()
 
